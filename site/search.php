9d092605be49190c939536d992109a94c083dcb0
PhantomVasploit <paulvasgit99@gmail.com>
2024-05-04 15:09:31 +0100

    start

---
+++ site/search.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Search results</title>
+    <meta charset="utf-8">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+    <meta name="description" content="Your description">
+    <meta name="keywords" content="Your keywords">
+    <meta name="author" content="Your name">    
+    <link rel="stylesheet" href="css/bootstrap.css" >
+    <link rel="stylesheet" href="css/responsive.css" >
+    <link rel="stylesheet" href="css/style.css" >
+		<script src="js/jquery.js"></script>
+        <script src="js/jquery.easing.1.3.js"></script>
+        <script src="js/jquery-migrate-1.1.1.js"></script>
+        <script src="js/superfish.js"></script>
+        <script src="js/jquery.mobilemenu.js"></script>
+         
+        <script src="js/jquery.ui.totop.js"></script>
+        <script src="search/search.js"></script>      
+      <!--[if lt IE 8]>
+          <div style='text-align:center'><a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://www.theie6countdown.com/images/upgrade.jpg"border="0"alt=""/></a></div>  
+      <![endif]-->
+  <!--[if lt IE 9]>
+     
+    <link rel="stylesheet" href="css/ie.css" >
+    <script src="assets/js/html5shiv.js"></script>
+  <![endif]-->
+</head>
+<body>    
+  <header class="subpages">  
+    <div class="container">
+      <div class="row">
+          <div class="span12 header-block">
+              <h1 class="brand"><a href="index.html"><img src="img/logo.png" alt="Vireta co."></a></h1> 
+              <ul class="soc-icons">
+                <li class="icon1"><a href="#"></a></li>
+                <li class="icon2"><a href="#"></a></li>
+                <li class="icon3"><a href="#"></a></li>
+                <li class="icon4"><a href="#"></a></li>
+                <li class="icon5"><a href="#"></a></li>
+             </ul>
+             <div class="clear clearfix2"></div>
+             <div class="navbar navbar_">
+              <div class="navbar-inner">                                                 
+                <div class="nav-collapse nav-collapse_ collapse">
+                  <ul class="nav sf-menu clearfix">
+                    <li><a href="index.html">home</a></li>
+                    <li class="sub-menu"><a href="index-1.html">about us</a>
+                      <ul>
+                        <li><a href="#">Vestibulum iaculis</a></li>
+                        <li class="sub-menu"><a href="#">Fusce euismod conuat</a>
+                        	<ul>
+                              <li><a href="#">Pellentesque sed</a></li>
+                              <li><a href="#">Aliquam congue ferm</a></li>
+                              <li><a href="#">Mauris accum</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Pellentesque</a></li>
+                      </ul>
+                    </li>
+                    <li><a href="index-2.html">Products</a></li>
+                    <li><a href="index-3.html">Contacts</a></li>
+                    <li><a href="index-4.html">Blog</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>   
+   </div>   
+  </header>
+  <!--==============================content=================================-->
+  <div id="content">
+    <div class="container pad-2">
+      <div class="row">
+          <div class="span12">
+              <h3>Search result:</h3>
+              <div id="search-results"></div>
+          </div>
+       </div> 
+    </div>    
+</div>  
+<!--==============================footer=================================-->
+<footer>
+  <div class="container">
+  	<div class="row">
+          <div class="span12"><span>Vireta co.</span> Agricultural Theme &copy;2013 | <a href="index-5.html">Privacy policy</a></div>
+  	</div> 
+  </div>
+</footer>
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
